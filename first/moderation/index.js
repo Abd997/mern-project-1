e0d93fc08af2fb6ce912d5683ac18377f40407a1
Abd <abdullahashfaq997@gmail.com>
2021-11-23 23:20:32 +0500

    start

---
+++ first/moderation/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+
+const app = express();
+const port = 4003;
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        });
+    }
+
+    res.send({});
+});
+
+app.listen(port, () => {
+console.log(`Active on port ${port}`);
+})
---
+++ first/query-service/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    handleEvents(type, data);
+    res.send({});
+});
+
+
+const handleEvents = (type, data) => {
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+        posts[id] = { id, title, comments: [] };
+    }
+    if (type === 'CommentCreated') {
+        const { id, content, postId, status } = data;
+        const post = posts[postId];
+        post.comments.push({ id, content, status });
+    }
+    if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data;
+        const post = posts[postId];
+
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+        comment.content = content;
+    }
+}
+
+app.listen(4002, async () => {
+    console.log("Active on port 4002");
+    try {
+        await axios.get('http://localhost:4005/events', (req, res) => {
+            for (let event of res.data) {
+                console.log('Processing event: ', event.type);
+                handleEvents(event.type, event.data);
+            }
+        });
+    } catch (error) {
+        console.error(error.response.data);
+    }
+
+});
